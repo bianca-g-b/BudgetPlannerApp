468fe15eacea4192227b54d4453dda33c32b9e56
Bianca <bianca.g.buha@gmail.com>
2023-09-04 23:58:50 +0100

    set up migrations and admin.py

--- backend/budget_planner/admin.py
+++ backend/budget_planner/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Budget
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'date_created', 'date_updated', 'total_income', 'housing', 'utility_bills', 'food_drinks', 'transport', 'household_goods_services', 'children_related_costs', 'cleaning_toiletries', 'other_essential_costs', 'luxury_gifts', 'leisure_entertainment', 'holidays', 'other_non_essential_costs', 'unsecured_loans')
 
 # Register your models here.
+admin.site.register(Budget, BudgetAdmin)
