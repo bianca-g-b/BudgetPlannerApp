3129ae846f55d19da60de4176f42d87ba5b152a2
Bianca <bianca.g.buha@gmail.com>
2023-09-06 14:56:04 +0100

    created views for signin and added html template

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create views here
 
@@ -16,10 +16,30 @@ def signup(request):
             password = form.cleaned_data["password1"]
             user = authenticate(username = username, password = password)
             login(request, user)
-            return redirect("/api")
+            return redirect("/api/budget")
         else:
             form = UserCreationForm()
             return render(request, "budget_planner/signup.html", {"form": form})
     else:
         form = UserCreationForm()
         return render(request, "budget_planner/signup.html", {"form": form})
+    
+
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect("/api/budget")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/api/budget")
+        else:
+            form = AuthenticationForm()
+            return render(request, "budget_planner/signin.html", {"form": form})
+    else:
+        form = AuthenticationForm()
+        return render(request, "budget_planner/signin.html", {"form": form})
+    
