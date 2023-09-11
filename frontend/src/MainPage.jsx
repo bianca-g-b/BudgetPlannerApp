52f588b1ef7a351b2380212f5ce3913d090ced2a
Bianca <bianca.g.buha@gmail.com>
2023-09-11 17:08:40 +0100

    added styling to the MainPage form

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -4,13 +4,15 @@ import Button from 'react-bootstrap/Button';
 
 function MainPage() {
     return (
-        <>
-            <Form>
+            <Form className = "full-form-area">
+            <h3 className = "app-title">Spendings tracker</h3><br/>
+
             <Form.Label htmlFor="basic-url"
-                variant = "dark"
             >Total income:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                    className="income-input"
+                >£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
@@ -18,79 +20,90 @@ function MainPage() {
 
             <Form.Label htmlFor="basic-url">Housing(rent, mortgage, etc):</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                    className="essential-input"
+                >£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Utilities(electricity, water, etc):</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text className="essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Transportation(gas, bus, etc):</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Household goods and services:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Children related(expenses, etc):</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Cleaning and toiletries:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Other essential expenses:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Luxury and gifts:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="non-essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Leisure and entertainment:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="non-essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Holidays expenses:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="non-essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Other non-essential expenses:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="non-essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
             <Form.Label htmlFor="basic-url">Unsecured debt:</Form.Label>
             <InputGroup className="mb-3">
-                <InputGroup.Text>£</InputGroup.Text>
+                <InputGroup.Text
+                className="non-essential-input">£</InputGroup.Text>
                 <Form.Control aria-label="Amount" />
             </InputGroup>
 
-            <Button variant="outline-primary" type="submit">Submit</Button>
+            <Button variant="primary" type="submit">Submit</Button>
             </Form>
-        </>
     )
 }
 
