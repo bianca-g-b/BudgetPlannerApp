ab7a826e4c782805d2e59be232d49ef8fff117e0
Bianca <bianca.g.buha@gmail.com>
2024-01-03 18:17:57 +0000

    added test for logout

--- backend/budget_planner/tests/test_views.py
+++ backend/budget_planner/tests/test_views.py
@@ -14,4 +14,6 @@ class BudgetTests(TestCase):
     def test_login(self):
         response = self.client.post("/auth/signin",  self.credentials, content_type="application/json")
         self.assertEqual(response.status_code, 202)
+        response = self.client.post("/auth/signout", content_type="application/json")
+        self.assertEqual(response.status_code, 202)
 
