8305b3bdea80515f8463e0c953c5d4d43ccdd1ef
Bianca <bianca.g.buha@gmail.com>
2024-03-14 20:05:53 +0000

    started building the auth forms in the backend

--- backend/authentication/forms.py
+++ backend/authentication/forms.py
@@ -1,13 +1,18 @@
 from django import forms
 from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
 
-# Create form for CustomUser model
+# Forms
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2')
 
-# class UserForm(forms.ModelForm):
-#     class Meta():
-#         model=CustomUser
-#         fields = (
-#             'username',
-#             'email',
-#             'password',
-#         )
+    def clean(self):
+        cleaned_data = super(SignupForm, self).clean()
+        password = cleaned_data.get('password1')
+        confirmPassword = cleaned_data.get('password2')
+
+        if password != confirmPassword:
+            raise forms.ValidationError("password and confirm_password does not match")
+        return cleaned_data
